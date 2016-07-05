05c0962e7a3cef081b366304b52bb4513e2ec567
Sam Corcos <sam@corcos.io>
2016-07-05 15:23:01 -0700

    adding classifiers

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
   url = 'https://github.com/POVME/POVME',
   download_url = 'https://github.com/POVME/POVME/tarball/2.0',
   keywords = ['protein folding', 'protein', 'pocket volume measurer', 'computational protein folding', 'protein structures', 'drug design'],
-  classifiers = [],
+  classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Scientific/Engineering :: Chemistry"
+  ],
 )
