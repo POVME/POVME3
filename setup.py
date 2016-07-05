6b9e978ae8999e0e70a312fbebca1460c790aaa9
Sam Corcos <sam@corcos.io>
2016-07-05 15:17:59 -0700

    updating config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'povme',
+  packages = ['povme'],
+  version = '2.0',
+  description = 'POVME (Pocket VOlume MEasurer) is a Python package for extracting actionable information from ensembles of protein structures for use in drug design.',
+  author = 'Jeff Wagner',
+  author_email = 'jwagnerjpl@gmail.com',
+  url = 'https://github.com/POVME/POVME',
+  download_url = 'https://github.com/POVME/POVME/tarball/2.0',
+  keywords = ['protein folding', 'protein', 'pocket volume measurer', 'computational protein folding', 'protein structures', 'drug design'],
+  classifiers = [],
+)
