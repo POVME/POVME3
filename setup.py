000adb8558785fc25a0033b332199b7c50789fd3
j-wags <jwagnerjpl@gmail.com>
2016-07-06 19:17:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
   author_email = 'jwagnerjpl@gmail.com',
   url = 'https://github.com/POVME/POVME',
   download_url = 'https://github.com/POVME/POVME/tarball/2.0',
+  install_requires=['numpy','scipy'],
   keywords = ['protein folding', 'protein', 'pocket volume measurer', 'computational protein folding', 'protein structures', 'drug design'],
   classifiers = [
     "Development Status :: 4 - Beta",
