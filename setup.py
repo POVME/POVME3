1a90d82280adc5d85a3df900b96c807628fb7eaa
j-wags <jwagnerjpl@gmail.com>
2016-07-07 08:55:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
   author_email = 'jwagnerjpl@gmail.com',
   url = 'https://github.com/POVME/POVME',
   download_url = 'https://github.com/POVME/POVME/tarball/2.0',
-  #setup_requires=['scipy','numpy'],
+  setup_requires=['scikit-learn'],
   #build_requires=['scipy','numpy'],
-  install_requires=['scipy'],
+  #install_requires=[''],
   keywords = ['protein folding', 'protein', 'pocket volume measurer', 'computational protein folding', 'protein structures', 'drug design'],
   classifiers = [
     "Development Status :: 4 - Beta",
