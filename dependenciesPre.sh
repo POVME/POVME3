7ac94f2689014dd0669d9acdb0e0686986d7b2b6
j-wags <jwagnerjpl@gmail.com>
2016-07-07 12:18:18 -0700

    Update dependenciesPre.sh

--- dependenciesPre.sh
+++ dependenciesPre.sh
@@ -1,27 +1,43 @@
 echo cd
 cd
+echo
+echo
 
 echo mkdir -p download
 mkdir -p download
+echo
+echo
 
 echo cd download
 cd download
+echo
+echo
 
 echo echo "Cached in $HOME/download :"
 echo "Cached in $HOME/download :"
+echo
+echo
 
 echo ls -l
 ls -l
+echo
+echo
 
 
 echo wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+echo
+echo
 
 echo chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
 chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
+echo
+echo
 
 echo cd ..
 cd ..
+echo
+echo
 
 #echo export PATH="$HOME/miniconda/bin:$PATH"
 #export PATH="$HOME/miniconda/bin:$PATH"
@@ -29,8 +45,13 @@ cd ..
 
 echo conda update --yes --quiet conda
 conda update --yes --quiet conda
+echo
+echo
 
+echo conda install --yes python numpy scipy 
 conda install --yes python numpy scipy 
+echo
+echo
 
 #echo conda create -n testenv --yes --quiet python numpy scipy cython nose coverage matplotlib sphinx pillow
 #conda create -n testenv --yes --quiet python numpy scipy cython nose coverage matplotlib sphinx pillow
@@ -38,7 +59,8 @@ conda install --yes python numpy scipy
 #echo source activate testenv
 #source activate testenv
 
-echo pip install .
-pip install .
+#echo pip install .
+#pip install .
+#echo
+#echo
 #sudo apt-get install python-numpy python-scipy
-echo 
