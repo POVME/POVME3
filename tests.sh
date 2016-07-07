f3fea2d23eadf014f27867c678e2010784088931
j-wags <jwagnerjpl@gmail.com>
2016-07-07 15:47:18 -0700

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 deactivate
 cd POVME/tests
-cd consistent_convex_hull_first
-../../arun python ../../POVME3.py sample_input.ini
-cd ../
-#python ./runRegTests.py
+#cd consistent_convex_hull_first
+#../../arun python ../../POVME3.py sample_input.ini
+#cd ../
+python ./runRegTests.py
 cd ../../
