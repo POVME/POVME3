1e775c9e4a990c5593ac1794525c3e9a50f62260
j-wags <jwagnerjpl@gmail.com>
2016-07-06 18:36:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-  name = 'POVME',
-  packages = ['POVME'],
+  name = 'povme',
+  packages = ['povme'],
   version = '3.0',
   description = 'POVME (Pocket VOlume MEasurer) is a Python package for extracting actionable information from ensembles of protein structures for use in drug design.',
   author = 'Jeff Wagner',
