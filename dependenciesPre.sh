9d3dede247863dcf4d35058c98b81fdd535def92
j-wags <jwagnerjpl@gmail.com>
2016-07-07 12:06:20 -0700

    Update dependenciesPre.sh

--- dependenciesPre.sh
+++ dependenciesPre.sh
@@ -13,6 +13,7 @@ echo "Cached in $HOME/download :"
 echo ls -l
 ls -l
 
+
 echo wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 
@@ -25,14 +26,17 @@ cd ..
 #echo export PATH="$HOME/miniconda/bin:$PATH"
 #export PATH="$HOME/miniconda/bin:$PATH"
 
+
 echo conda update --yes --quiet conda
 conda update --yes --quiet conda
 
-echo conda create -n testenv --yes --quiet python numpy scipy cython nose coverage matplotlib sphinx pillow
-conda create -n testenv --yes --quiet python numpy scipy cython nose coverage matplotlib sphinx pillow
+conda install python numpy scipy cython nose coverage matplotlib sphinx pillow
 
-echo source activate testenv
-source activate testenv
+#echo conda create -n testenv --yes --quiet python numpy scipy cython nose coverage matplotlib sphinx pillow
+#conda create -n testenv --yes --quiet python numpy scipy cython nose coverage matplotlib sphinx pillow
+
+#echo source activate testenv
+#source activate testenv
 
 echo pip install .
 pip install .
